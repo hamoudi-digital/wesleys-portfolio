cbe8d8d4b63f431f79184064d98a6114f7ba8412
hamoudi-digital <hamoudidigital@gmail.com>
2023-10-03 08:31:41 -0400

    new images

--- src/components/gallery.js
+++ src/components/gallery.js
@@ -5,6 +5,31 @@ import 'owl.carousel/dist/assets/owl.theme.default.css';
 
 function Gallery() {
     const gallery1 = require('../assets/images/_DSF0239.jpg');
+    const gallery2 = require('../assets/images/_DSF0372.jpg');
+    const gallery3 = require('../assets/images/_DSF0409.jpg');
+    const gallery4 = require('../assets/images/_DSF0411.jpg');
+    const gallery5 = require('../assets/images/_DSF0415.jpg');
+    const gallery6 = require('../assets/images/_DSF0426.jpg');
+    const gallery7 = require('../assets/images/_DSF0454.jpg');
+    const gallery8 = require('../assets/images/_DSF0459.jpg');
+    const gallery9 = require('../assets/images/_DSF0617.jpg');
+    const gallery10 = require('../assets/images/_DSF0630.jpg');
+    const gallery11 = require('../assets/images/107A1196.jpg');
+    const gallery12 = require('../assets/images/45613C30-0578-4E9C-B2E2-97DB4FA838A3 (1).jpg');
+    const gallery13 = require('../assets/images/C52A56EE-BF0E-48AD-A84A-60C25B806850.jpg');
+    const gallery14 = require('../assets/images/DSC_3793-1_Original.jpg');
+    const gallery15 = require('../assets/images/DSC_4303-117_Original.jpg');
+    const gallery16 = require('../assets/images/F3366E7E-89B3-4B62-A6BD-20B50AF499B7.jpg');
+    const gallery17 = require('../assets/images/IMG_5180.jpg');
+    const gallery18 = require('../assets/images/IMG_5540.jpg');
+    const gallery19 = require('../assets/images/IMG_5569.jpg');
+    const gallery20 = require('../assets/images/IMG_5572.jpg');
+    const gallery21 = require('../assets/images/IMG_5593 (1).jpg');
+    const gallery22 = require('../assets/images/IMG_5593.jpg');
+    const gallery23 = require('../assets/images/IMG_5598.jpg');
+    const gallery24 = require('../assets/images/IMG_5601.jpg');
+    const gallery25 = require('../assets/images/IMG_5602.jpg');
+    const gallery26 = require('../assets/images/jam-new-haven.jpg');
 
     return(
         <div className='section gallery' id='gallery'>
@@ -34,30 +59,77 @@ function Gallery() {
                                     <img className="img" src={gallery1}/>
                                 </div> 
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery2}/>
                                 </div>
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery3}/>
                                 </div>
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery4}/>
                                 </div>
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery5}/>
                                 </div>
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery6}/>
                                 </div>
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery7}/>
                                 </div>
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery8}/>
                                 </div>
                                 <div className="gallery-item">
-                                    <img className="img" src={gallery1}/>
+                                    <img className="img" src={gallery9}/>
                                 </div>
-
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery10}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery11}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery12}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery13}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery14}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery15}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery16}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery17}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery18}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery19}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery20}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery21}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery22}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery23}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery24}/>
+                                </div>
+                                <div className="gallery-item">
+                                    <img className="img" src={gallery25}/>
+                                </div>
                             </OwlCarousel>  
                         </div>
                     </div>
